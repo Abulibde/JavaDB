1ab96d4b46fa3de4d600f0cbf12b66ed913b17a7
Abu <iliyanananan@gmail.com>
2022-09-21 10:07:29 +0300

    softUni database -> 11. Find All Employees with salary More Than 50000

--- MySql/2) Basic CRUD/soft_uni_database.sql
+++ MySql/2) Basic CRUD/soft_uni_database.sql
@@ -1971,6 +1971,7 @@ WHERE `manager_id` IS NULL;
 # 11. Find All Employees with salary More Than 50000
 SELECT `first_name`,`last_name`,`salary`
 FROM `employees`
-WHERE `salary` > 50000;
+WHERE `salary` > 50000
+ORDER BY `salary` DESC;
 
 
