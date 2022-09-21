4e526689de7f3b0c185aabf64fba86f1b592b17d
Abu <iliyanananan@gmail.com>
2022-09-21 10:09:33 +0300

    softUni database ->  Find 5 Best Paid Employees

--- MySql/2) Basic CRUD/soft_uni_database.sql
+++ MySql/2) Basic CRUD/soft_uni_database.sql
@@ -1974,4 +1974,11 @@ FROM `employees`
 WHERE `salary` > 50000
 ORDER BY `salary` DESC;
 
+# 12. Find 5 Best Paid Employees
+SELECT `first_name`,`last_name`
+FROM `employees`
+ORDER BY `salary` DESC
+LIMIT 5;
+
+
 
