5c4b14215a85be6b706ca0b7e4cdcc30eff9fbe9
Abu <iliyanananan@gmail.com>
2022-09-21 10:12:33 +0300

    softUni database ->  Find 5 Best Paid Employees

--- MySql/2) Basic CRUD/soft_uni_database.sql
+++ MySql/2) Basic CRUD/soft_uni_database.sql
@@ -1980,7 +1980,10 @@ FROM `employees`
 ORDER BY `salary` DESC
 LIMIT 5;
 
-#
+# 13. Find All Employees Except Marketing
+SELECT `first_name`,`last_name`
+FROM `employees`
+WHERE `department_id` != 4;
 
 
 
