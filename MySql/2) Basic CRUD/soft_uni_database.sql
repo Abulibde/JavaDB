e406d4e56efcef2ef5264b7ef0cf04b567f6ff76
Abu <iliyanananan@gmail.com>
2022-09-22 09:00:50 +0300

    softUni database ->  20. Increase Salaries

--- MySql/2) Basic CRUD/soft_uni_database.sql
+++ MySql/2) Basic CRUD/soft_uni_database.sql
@@ -2033,9 +2033,10 @@ ORDER BY `hire_date` DESC
 LIMIT 7;
 
 # 20. Increase Salaries
+#1,2,4,11
+UPDATE `employees`
+SET `salary` = `salary` * 1.12
+WHERE `department_id` IN (1,2,4,11);
 
-
-
-
-
-
+SELECT `salary`
+FROM `employees`;
