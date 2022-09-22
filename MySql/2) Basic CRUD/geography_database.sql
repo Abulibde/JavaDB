e451f3a84e615c1de08f87de0011d3ab69e42f9d
Abu <iliyanananan@gmail.com>
2022-09-22 09:11:05 +0300

    softUni database ->  22.  Biggest Countries by Population

--- MySql/2) Basic CRUD/geography_database.sql
+++ MySql/2) Basic CRUD/geography_database.sql
@@ -808,3 +808,9 @@ FROM `peaks`
 ORDER BY `peak_name`;
 
 # 22.  Biggest Countries by Population
+SELECT `country_name`,`population`
+FROM countries
+WHERE `continent_code` = 'EU'
+ORDER BY `population`DESC,
+         `country_name`
+LIMIT 30;
