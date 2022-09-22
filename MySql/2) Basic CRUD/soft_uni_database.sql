ca4819f53a3fc4ded3d497e19028550aee507909
Abu <iliyanananan@gmail.com>
2022-09-22 08:48:26 +0300

    softUni database ->  18. Find First 10 Started Projects

--- MySql/2) Basic CRUD/soft_uni_database.sql
+++ MySql/2) Basic CRUD/soft_uni_database.sql
@@ -2019,6 +2019,13 @@ FROM `employees`
 ORDER BY `job_title`;
 
 # 18. Find First 10 Started Projects
+SELECT *
+FROM `projects`
+ORDER BY `start_date`,
+         `name`,
+         `project_id`
+LIMIT 10;
+
 
 
 
