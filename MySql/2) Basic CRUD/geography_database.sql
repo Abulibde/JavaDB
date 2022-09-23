5e894814218f8a34b8c440f0e8f7a5201a91eef0
Abu <iliyanananan@gmail.com>
2022-09-23 08:21:33 +0300

    softUni database ->  23.  Countries and Currency (Euro / Not Euro)

--- MySql/2) Basic CRUD/geography_database.sql
+++ MySql/2) Basic CRUD/geography_database.sql
@@ -852,3 +852,10 @@ ORDER BY `population` DESC,
 LIMIT 30;
 
 # 23.  Countries and Currency (Euro / Not Euro)
+SELECT `country_name`,
+       `country_code`,
+       IF(`currency_code` = 'EUR', 'Euro', 'Not Euro') AS 'currency'
+FROM `countries`
+ORDER BY `country_name`;
+
+#
