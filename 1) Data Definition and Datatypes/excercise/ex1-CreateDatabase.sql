e6c7f1026a3f05d12fb28d1a29c389afda7a7321
Abu <iliyanananan@gmail.com>
2022-09-27 10:08:05 +0300

    first file in the directory

--- 1) Data Definition and Datatypes/excercise/ex1-CreateDatabase.sql
+++ 1) Data Definition and Datatypes/excercise/ex1-CreateDatabase.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE `minions`
+
+USE `minions`;
+CREATE TABLE `minions`(
+                          id INT PRIMARY KEY AUTO_INCREMENT,
+                          name VARCHAR(255),
+                          age INT
+);
+
+USE `minions`;
+CREATE TABLE `towns`(
+                        town_id INT PRIMARY KEY AUTO_INCREMENT,
+                        name VARCHAR(255) NOT NULL
+);
