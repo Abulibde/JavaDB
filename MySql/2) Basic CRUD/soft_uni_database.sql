b83b6c1325cad35cd9a8e42af56af1113520d36a
Abu <iliyanananan@gmail.com>
2022-09-27 19:38:21 +0300

    softUni database -> find all info about Departments

--- MySql/2) Basic CRUD/soft_uni_database.sql
+++ MySql/2) Basic CRUD/soft_uni_database.sql
@@ -1672,3 +1672,10 @@ INSERT INTO `towns` (`town_id`, `name`) VALUES
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+
+#1. Find All Information About Departments
+USE soft_uni;
+SELECT *
+FROM `departments`
+ORDER BY `department_id`;
