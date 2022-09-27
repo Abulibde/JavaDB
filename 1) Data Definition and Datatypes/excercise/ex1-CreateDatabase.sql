0bcd2612c3f885f81434f0a2d5813befce44aadf
Abu <iliyanananan@gmail.com>
2022-09-27 11:27:02 +0300

    Create table People

--- 1) Data Definition and Datatypes/excercise/ex1-CreateDatabase.sql
+++ 1) Data Definition and Datatypes/excercise/ex1-CreateDatabase.sql
@@ -49,4 +49,25 @@ TRUNCATE TABLE `minions`;
 DROP TABLE `minions`;
 DROP TABLE `towns`;
 
+#Create table people
+CREATE TABLE `people`
+(
+    `id`        INT PRIMARY KEY AUTO_INCREMENT,
+    `name`      VARCHAR(200) NOT NULL,
+    `picture`   BLOB,
+    `height`    DOUBLE(10, 2),
+    `weight`    DOUBLE(10, 2),
+    `gender`    CHAR(1)      NOT NULL,
+    `birthdate` DATE         NOT NULL,
+    `biography` TEXT
+);
+
+INSERT INTO `people`(`name`,`gender`,`birthdate`)
+VALUES ('Boris','m',DATE (NOW())),
+       ('Nasko','m',DATE (NOW())),
+       ('Peter','m',DATE (NOW())),
+       ('Ivanka','f',DATE (NOW())),
+       ('Chiko','m',DATE (NOW()));
+
+
 
