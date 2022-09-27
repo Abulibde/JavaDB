143117fef710c59a6c871c71bc04357ba2ceaa0c
Abu <iliyanananan@gmail.com>
2022-09-27 14:04:31 +0300

    Select all fields

--- 1) Data Definition and Datatypes/excercise/ex1-CreateDatabase.sql
+++ 1) Data Definition and Datatypes/excercise/ex1-CreateDatabase.sql
@@ -156,14 +156,24 @@ VALUES ('Engineering'),
        ('Software Development'),
        ('Quality Assurance');
 
-
-
 INSERT INTO `employees` (`first_name`, `middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`, `salary`)
 VALUES ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013/02/01', 3500.00),
        ('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004/03/02', 4000.00),
        ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016/08/28', 525.25),
        ('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007/12/09', 3000.00),
        ('Peter', 'Pan', 'Pan', 'Intern', 3, '2016/08/28', 599.88);
+
+# 14. Basic Select All Fields
+#14.1 Select all field from towns
+SELECT * FROM `towns`;
+
+#14.2 Select all fields from departments
+SELECT * FROM `departments`;
+
+#14.3 Select all fields from employees
+SELECT * FROM `employees`;
+
+
 
 
 
