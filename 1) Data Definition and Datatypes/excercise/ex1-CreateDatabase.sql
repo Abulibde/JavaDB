6197da4f1404def4351ba3194e32e46fd85e7a7b
Abu <iliyanananan@gmail.com>
2022-09-27 13:57:51 +0300

    Basic input

--- 1) Data Definition and Datatypes/excercise/ex1-CreateDatabase.sql
+++ 1) Data Definition and Datatypes/excercise/ex1-CreateDatabase.sql
@@ -105,6 +105,69 @@ ALTER TABLE `users`
         PRIMARY KEY `users` (`id`),
     MODIFY COLUMN `username` VARCHAR(30) UNIQUE;
 
+#13 Basic insert
+CREATE DATABASE `soft_uni`;
+USE `soft_uni`;
+
+#13.1 create tables
+CREATE TABLE `towns`
+(
+    `id`   INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `addresses`
+(
+    `id`          INT PRIMARY KEY AUTO_INCREMENT,
+    `addres_text` VARCHAR(255),
+    `town_id`     INT NOT NULL
+);
+
+CREATE TABLE `departments`
+(
+    `id`   INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `employees`
+(
+    `id`            INT PRIMARY KEY AUTO_INCREMENT,
+    `first_name`    VARCHAR(255) NOT NULL,
+    `middle_name`   VARCHAR(255) NOT NULL,
+    `last_name`     VARCHAR(255) NOT NULL,
+    `job_title`     VARCHAR(255) NOT NULL,
+    `department_id` INT          NOT NULL,
+    `hire_date`     DATE,
+    `salary`        DECIMAL,
+    `address_id`    INT          NOT NULL
+);
+
+#13.2 insert into tables
+INSERT INTO `towns`(`name`)
+VALUES ('Sofia'),
+       ('Plovdiv'),
+       ('Varna'),
+       ('Burgas');
+
+INSERT INTO `departments`(`name`)
+VALUES ('Engineering'),
+       ('Sales'),
+       ('Marketing'),
+       ('Software Development'),
+       ('Quality Assurance');
+
+
+
+INSERT INTO `employees` (`first_name`, `middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`, `salary`)
+VALUES ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013/02/01', 3500.00),
+       ('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004/03/02', 4000.00),
+       ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016/08/28', 525.25),
+       ('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007/12/09', 3000.00),
+       ('Peter', 'Pan', 'Pan', 'Intern', 3, '2016/08/28', 599.88);
+
+
+
+
 
 
 
