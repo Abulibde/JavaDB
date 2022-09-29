526dffafff79b23939bf3a2a4e8cc3145a463371
Abu <iliyanananan@gmail.com>
2022-09-29 19:50:27 +0300

    Gringotts database -> 4.     Smallest Deposit Group Per Magic Wand Size*

--- MySql/4) Data Aggregation/gringotts-database.sql
+++ MySql/4) Data Aggregation/gringotts-database.sql
@@ -452,8 +452,19 @@ FROM `wizzard_deposits`
 GROUP BY `deposit_group`
 ORDER BY `longest_magic_wand`, `deposit_group`;
 
+# 4.     Smallest Deposit Group Per Magic Wand Size*
+SELECT `deposit_group`
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY AVG(`magic_wand_size`)
+LIMIT 1;
+
+
+
+
 # 5.	 Deposits Sum
 SELECT `deposit_group`, SUM(`deposit_amount`) AS 'sum amount'
 FROM `wizzard_deposits`
 GROUP BY `deposit_group`
 ORDER BY `sum amount`;
+
