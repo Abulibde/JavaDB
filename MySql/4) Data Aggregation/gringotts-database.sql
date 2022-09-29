9d77546c16fcce2ae528a525af97bf3a56935ea5
Abu <iliyanananan@gmail.com>
2022-09-29 20:07:31 +0300

    Gringotts database -> 8. Deposit Charge

--- MySql/4) Data Aggregation/gringotts-database.sql
+++ MySql/4) Data Aggregation/gringotts-database.sql
@@ -481,5 +481,10 @@ HAVING `total_sum` < 150000
 ORDER BY `total_sum` DESC;
 
 # 8.    Deposit Charge
-
+SELECT `deposit_group`,
+       `magic_wand_creator`,
+       MIN(`deposit_charge`) AS 'min_deposit_charge'
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`, `magic_wand_creator`
+ORDER BY `magic_wand_creator`, `deposit_group`;
 
