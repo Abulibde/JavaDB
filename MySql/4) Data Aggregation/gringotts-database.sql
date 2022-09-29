3da21cfadc19a27dba74e2001f326479337e4091
Abu <iliyanananan@gmail.com>
2022-09-29 20:00:27 +0300

    Gringotts database -> 7.	Deposits Filter

--- MySql/4) Data Aggregation/gringotts-database.sql
+++ MySql/4) Data Aggregation/gringotts-database.sql
@@ -473,4 +473,10 @@ GROUP BY `deposit_group`
 ORDER BY `deposit_group`;
 
 # 7.    Deposits Filter
+SELECT `deposit_group`, SUM(`deposit_amount`) AS 'total_sum'
+FROM `wizzard_deposits`
+WHERE `magic_wand_creator` = 'Ollivander family'
+GROUP BY `deposit_group`
+HAVING `total_sum` < 150000
+ORDER BY `total_sum` DESC;
 
