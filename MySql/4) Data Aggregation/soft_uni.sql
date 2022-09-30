5e4ad087f4dd803e01cdaf018f3192b23d9a36b3
Abu <iliyanananan@gmail.com>
2022-09-30 14:44:27 +0300

    SoftUni database -> # 16.	3rd Highest Salary*

--- MySql/4) Data Aggregation/soft_uni.sql
+++ MySql/4) Data Aggregation/soft_uni.sql
@@ -1954,4 +1954,13 @@ FROM `employees`
 WHERE `manager_id` IS NULL;
 
 # 16.	3rd Highest Salary*
-
+SELECT `department_id`,
+       (SELECT DISTINCT `salary`
+        FROM `employees` e
+        WHERE e.`department_id` = `employees`.`department_id`
+        ORDER BY `salary` DESC
+        LIMIT 1 OFFSET 2) AS 'third_highest_salary'
+FROM `employees`
+GROUP BY `department_id`
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `department_id`;
