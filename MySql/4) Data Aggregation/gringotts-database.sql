1b7b76d0756f3ff8693d211d8de905cae071efe7
Abu <iliyanananan@gmail.com>
2022-09-30 11:38:30 +0300

    Gringotts database -> 11.	Average Interest

--- MySql/4) Data Aggregation/gringotts-database.sql
+++ MySql/4) Data Aggregation/gringotts-database.sql
@@ -511,5 +511,9 @@ GROUP BY `first_letter`
 ORDER BY `first_letter`;
 
 # 11.	Average Interest
-
+SELECT deposit_group, is_deposit_expired, avg(deposit_interest) AS average_interest
+FROM wizzard_deposits
+WHERE deposit_start_date > '1985-01-01'
+GROUP BY deposit_group, is_deposit_expired
+ORDER BY deposit_group DESC, is_deposit_expired;
 
