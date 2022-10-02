06c6190128de03f63af45a278265ad67a941dc42
Abu <iliyanananan@gmail.com>
2022-10-02 13:57:27 +0300

    Table relations -> insert into passports table

--- MySql/5) Table Relations/Table relations.sql
+++ MySql/5) Table Relations/Table relations.sql
@@ -11,7 +11,16 @@ CREATE TABLE `people`
 
 CREATE TABLE `passports`
 (
-    `passport_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `passport_id`     INT AUTO_INCREMENT PRIMARY KEY,
     `passport_number` VARCHAR(255)
 );
 
+# passports
+# passport_id	passport_number
+#    101            N34FG21B
+#    102	        K65LO4R7
+#    103	        ZE657QP2
+INSERT INTO `passports`(`passport_id`, `passport_number`)
+VALUES (101, 'N34FG21B'),
+       (102, 'K65LO4R7'),
+       (103, 'ZE657QP2');
