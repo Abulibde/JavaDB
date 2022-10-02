5a419bfb0734d0f659ff5b764743c2e9bdef7a34
Abu <iliyanananan@gmail.com>
2022-10-02 17:56:32 +0300

    Table relations -> insert into models

--- MySql/5) Table Relations/Table relations.sql
+++ MySql/5) Table Relations/Table relations.sql
@@ -74,4 +74,20 @@ VALUES (1, 'BMW', '1916-03-01'),
        (2, 'Tesla', '2003-01-01'),
        (3, 'BMW', '1996-05-01');
 
+#           models
+# model_id      	name	manufacturer_id
+# 101	X1	1
+# 102	i6	1
+# 103	Model S	2
+# 104	Model X	2
+# 105	Model 3	2
+# 106	Nova	3
+
+INSERT INTO `models`
+VALUES (101, 'X1', 1),
+       (102, 'i6', 1),
+       (103, 'Model S', 2),
+       (104, 'Model X', 2),
+       (105, 'Model 3', 2),
+       (106, 'Nova', 3);
 
