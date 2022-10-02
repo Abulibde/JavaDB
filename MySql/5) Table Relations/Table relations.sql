15759b301c18a0be14e2d504b9a7b720483f95c8
Abu <iliyanananan@gmail.com>
2022-10-02 14:02:25 +0300

    Table relations -> insert into people table

--- MySql/5) Table Relations/Table relations.sql
+++ MySql/5) Table Relations/Table relations.sql
@@ -24,3 +24,14 @@ INSERT INTO `passports`(`passport_id`, `passport_number`)
 VALUES (101, 'N34FG21B'),
        (102, 'K65LO4R7'),
        (103, 'ZE657QP2');
+
+
+#                   people
+# person_id	        first_name	        salary	    passport_id
+#   1  	            Roberto            	43300.00	    102
+#   2	            Tom             	56100.00	    103
+#   3	            Yana	            60200.00	    101
+INSERT INTO `people` (`person_id`, `first_name`, `salary`, `passport_id`)
+VALUES (1, 'Roberto', 43300.00, 102),
+       (2, 'Tom', 56100.00, 103),
+       (3, 'Yana', 60200.00, 101);
