1c2797dea147b43e787d023d56ce5c75bfab8b4e
Abu <iliyanananan@gmail.com>
2022-10-06 22:12:31 +0300

    SoftUni database -> 2.	Addresses with Towns

--- MySql/6) Subqueries and JOINs/soft_uni_database.sql
+++ MySql/6) Subqueries and JOINs/soft_uni_database.sql
@@ -1924,3 +1924,10 @@ ORDER BY `address_id`
 LIMIT 5;
 
 # 2.	Addresses with Towns
+SELECT `first_name`, `last_name`, `name` AS 'town', `address_text`
+FROM employees AS e
+         JOIN addresses AS a ON a.address_id = e.address_id
+         JOIN towns AS t ON t.`town_id` = a.`town_id`
+ORDER BY `first_name`,
+         `last_name`
+LIMIT 5;
