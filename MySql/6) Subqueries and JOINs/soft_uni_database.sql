3cf134bb63da09908f56489729a23bf452ab8465
Abu <iliyanananan@gmail.com>
2022-10-06 21:40:25 +0300

    SoftUni database -> 1.	Employee Address

--- MySql/6) Subqueries and JOINs/soft_uni_database.sql
+++ MySql/6) Subqueries and JOINs/soft_uni_database.sql
@@ -1917,3 +1917,8 @@ VALUES (1, 'Redmond'),
 
 
 # 1.	Employee Address
+SELECT e.`employee_id`, e.`job_title`, e.`address_id`, a.`address_text`
+FROM employees AS e
+         JOIN addresses AS a ON a.address_id = e.address_id
+ORDER BY `address_id`
+LIMIT 5;
