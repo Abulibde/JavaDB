5f64c225b0e9e17251919289f9a7a8101d3cdc85
Abu <iliyanananan@gmail.com>
2022-10-07 15:50:56 +0300

    SoftUni database -> 7.	Employees with Project

--- MySql/6) Subqueries and JOINs/soft_uni_database.sql
+++ MySql/6) Subqueries and JOINs/soft_uni_database.sql
@@ -1964,4 +1964,11 @@ WHERE e.`hire_date` > '1990-01-01 00:00:00'
 ORDER BY hire_date;
 
 # 7.	Employees with Project
-
+SELECT e.`employee_id`, e.`first_name`, p.`name` AS 'project_name'
+FROM `employees` AS e
+         JOIN `employees_projects` AS ep ON e.`employee_id` = ep.`employee_id`
+         JOIN projects p on ep.project_id = p.project_id
+WHERE p.`start_date` > '2002-08-13'
+  AND p.`end_date` IS NULL
+ORDER BY e.`first_name`, p.`name`
+LIMIT 5;
