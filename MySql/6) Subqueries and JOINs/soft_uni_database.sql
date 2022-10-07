2ee83aa44305fffbdb5145ba0a01e142dfbada8b
Abu <iliyanananan@gmail.com>
2022-10-07 17:28:30 +0300

    SoftUni database -> 10.	Employee Summary

--- MySql/6) Subqueries and JOINs/soft_uni_database.sql
+++ MySql/6) Subqueries and JOINs/soft_uni_database.sql
@@ -1992,3 +1992,13 @@ WHERE e.`manager_id` = m.`employee_id`
 ORDER BY `first_name`;
 
 # 10.	Employee Summary
+SELECT e.`employee_id`,
+       CONCAT(e.`first_name`, ' ', e.`last_name`) AS 'employee_name',
+       CONCAT(m.`first_name`, ' ', m.`last_name`) AS 'manager_name',
+       d.`name`                                   AS 'department_name'
+FROM `employees` AS e
+         JOIN `employees` AS m ON e.`manager_id` = m.`employee_id`
+         JOIN `departments` AS d ON e.department_id = d.department_id
+WHERE e.manager_id IS NOT NULL
+ORDER BY `employee_id`
+LIMIT 5;
