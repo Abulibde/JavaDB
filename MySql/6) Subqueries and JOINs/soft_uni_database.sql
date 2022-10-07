30cb273a62fee62dc470c141eab84c14f2273dc8
Abu <iliyanananan@gmail.com>
2022-10-07 15:19:08 +0300

    SoftUni database -> 3.	Sales Employee

--- MySql/6) Subqueries and JOINs/soft_uni_database.sql
+++ MySql/6) Subqueries and JOINs/soft_uni_database.sql
@@ -1933,3 +1933,9 @@ ORDER BY `first_name`,
 LIMIT 5;
 
 # 3.	Sales Employee
+SELECT e.`employee_id`, e.`first_name`, e.`salary`, d.`name`
+FROM `employees` AS e
+JOIN departments AS d ON d.department_id = e.department_id
+WHERE salary > 15000
+ORDER BY d.`department_id`DESC
+LIMIT 5;
