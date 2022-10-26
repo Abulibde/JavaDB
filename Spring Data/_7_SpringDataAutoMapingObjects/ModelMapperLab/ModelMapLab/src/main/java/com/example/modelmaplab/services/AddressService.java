2158517e886720a9cf3bed6837f4b9262dd129bf
Abu <iliyanananan@gmail.com>
2022-10-26 09:43:35 +0300

    testing

--- Spring Data/_7_SpringDataAutoMapingObjects/ModelMapperLab/ModelMapLab/src/main/java/com/example/modelmaplab/services/AddressService.java
+++
@@ -1,9 +0,0 @@
-package com.example.modelmaplab.services;
-
-import com.example.modelmaplab.domain.DTO.CreateAddressDTO;
-import com.example.modelmaplab.domain.entities.Address;
-
-public interface AddressService {
-    Address create(CreateAddressDTO data);
-
-}
