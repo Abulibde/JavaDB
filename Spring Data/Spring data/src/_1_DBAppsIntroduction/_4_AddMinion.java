31a3c84773fd8983c729b6b635af67d2d6125331
Abu <iliyanananan@gmail.com>
2022-10-27 15:06:58 +0300

    4 -> Add minion

--- Spring Data/Spring data/src/_1_DBAppsIntroduction/_4_AddMinion.java
+++ Spring Data/Spring data/src/_1_DBAppsIntroduction/_4_AddMinion.java
@@ -3,7 +3,7 @@ package _1_DBAppsIntroduction;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class AddMinion {
+public class _4_AddMinion {
     public static void main(String[] args) throws SQLException {
         Connection connection = Utils.getSQLConnection();
 
