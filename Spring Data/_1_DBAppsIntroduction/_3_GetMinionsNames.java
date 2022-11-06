3825e5368c202cde416d28ce9e1b526ff9753599
Abu <iliyanananan@gmail.com>
2022-11-06 15:03:16 +0200

    refactor

---
+++ Spring Data/_1_DBAppsIntroduction/_3_GetMinionsNames.java
@@ -0,0 +1,65 @@
+package _1_DBAppsIntroduction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class _3_GetMinionsNames {
+    private static final String GET_MINIONS_NAME_AND_AGE_BY_VILLAIN_ID =
+            "SELECT m.name, m.age" +
+                    " FROM minions as m" +
+                    " JOIN minions_villains mv on m.id = mv.minion_id" +
+                    " WHERE villain_id = ?";
+    private static final String GET_VILLAIN_NAME_BY_ID =
+            "SELECT name" +
+                    " from villains " +
+                    " where id = ?";
+
+    private static final String COLUMN_LABEL_AGE = "age";
+    private static final String NO_VILLAIN_FORMAT = "No villain with ID %d exists in the database.";
+    private static final String VILLAIN_FORMAT = "Villain: %s%n";
+    private static final String MINION_FORMAT = "%d. %s %d%n";
+
+
+    public static void main(String[] args) throws SQLException {
+        final Connection connection = Utils.getSQLConnection();
+
+        final int villainId = new Scanner(System.in).nextInt();
+
+        final PreparedStatement villainStatement = connection.prepareStatement(GET_VILLAIN_NAME_BY_ID);
+
+        villainStatement.setInt(1, villainId);
+
+        ResultSet villainSet = villainStatement.executeQuery();
+
+
+        if (!villainSet.next()) {
+            System.out.printf(NO_VILLAIN_FORMAT, villainId);
+            connection.close();
+            return;
+        }
+
+
+        final String villainName = villainSet.getString(Constants.COLUMN_LABEL_NAME);
+
+        System.out.printf(VILLAIN_FORMAT, villainName);
+
+        final PreparedStatement minionsStatement = connection.prepareStatement(GET_MINIONS_NAME_AND_AGE_BY_VILLAIN_ID);
+
+        minionsStatement.setInt(1, villainId);
+
+        final ResultSet minionsSet = minionsStatement.executeQuery();
+
+        for (int index = 1; minionsSet.next(); index++) {
+            final String minionsName = minionsSet.getString(Constants.COLUMN_LABEL_NAME);
+            final int minionsAge = minionsSet.getInt(COLUMN_LABEL_AGE);
+
+            System.out.printf(MINION_FORMAT, index, minionsName, minionsAge);
+        }
+        connection.close();
+    }
+
+}
+
---
+++ Spring Data/_1_DBAppsIntroduction/_8_IncreaseMinionsAge.java
@@ -0,0 +1,8 @@
+package _1_DBAppsIntroduction;
+
+public class _8_IncreaseMinionsAge {
+    public static void main(String[] args) {
+        
+
+    }
+}
