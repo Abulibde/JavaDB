53391f2f7b3729eeaa21b5a558efbee78beb8339
Abu <iliyanananan@gmail.com>
2022-11-07 18:03:07 +0200

    8. Get employee with project

---
+++ Spring Data/_3_IntroductionToHibernate/Exercise/src/main/java/_8_GetEmployeeWithProject.java
@@ -0,0 +1,28 @@
+/*
+Get an employee by his/her id.
+Print only his/her first name, last name, job title and projects (only their names).
+The projects should be ordered by name (ascending).
+The output should be printed in the format given in the example.
+
+                            Example
+Input	                        Output
+147	                            Linda Randall - Production Technician
+	                            HL Touring Handlebars
+	                            ML Road Rear Wheel
+	                            Patch kit
+	                            Touring-1000
+
+
+83	                            John Evans - Production Technician
+	                            Half-Finger Gloves
+	                            LL Mountain Handlebars
+	                            Racing Socks
+	                            Women's Tights
+
+ */
+
+public class _8_GetEmployeeWithProject {
+    public static void main(String[] args) {
+
+    }
+}
