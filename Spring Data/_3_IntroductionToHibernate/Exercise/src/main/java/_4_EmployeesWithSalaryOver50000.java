0009d0622710ee258297081d3d4e5de62f84b6bd
Abu <iliyanananan@gmail.com>
2022-11-07 14:49:04 +0200

    ex4. Employees With Salary Over 50 000

---
+++ Spring Data/_3_IntroductionToHibernate/Exercise/src/main/java/_4_EmployeesWithSalaryOver50000.java
@@ -0,0 +1,8 @@
+//Write a program that gets the first name of all employees
+// who have salary over 50 000.
+
+public class _4_EmployeesWithSalaryOver50000 {
+    public static void main(String[] args) {
+
+    }
+}
