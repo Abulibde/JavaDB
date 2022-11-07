644d34f7037a137bc573116a5bdd5dcee0318962
Abu <iliyanananan@gmail.com>
2022-11-07 15:50:27 +0200

    6.	Adding a New Address and Updating Employee

---
+++ Spring Data/_3_IntroductionToHibernate/Exercise/src/main/java/_6_AddNewAddressAndUpdatingEmployee.java
@@ -0,0 +1,8 @@
+// Create a new address with text "Vitoshka 15".
+// Set that address to an employee with a last name, given as an input.
+
+public class _6_AddNewAddressAndUpdatingEmployee {
+    public static void main(String[] args) {
+
+    }
+}
