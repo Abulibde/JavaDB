9293319fa7c6942e8f340da02df6d0319750530a
Abu <iliyanananan@gmail.com>
2022-11-10 20:10:40 +0200

    insert new employee into database

--- Spring Data/_7_SpringDataAutoMapingObjects/ModelMapperLab/ModelMapLab/src/main/java/com/example/modelmaplab/repositories/AddressRepository.java
+++ Spring Data/_7_SpringDataAutoMapingObjects/ModelMapperLab/ModelMapLab/src/main/java/com/example/modelmaplab/repositories/AddressRepository.java
@@ -4,6 +4,9 @@ import com.example.modelmaplab.domain.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address,Long> {
+    Optional<Address> findByCountryAndCity(String country, String city);
 }
