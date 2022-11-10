d0912d4b14c985283c6130f2bcfa190780f291ef
Abu <iliyanananan@gmail.com>
2022-11-10 20:11:01 +0200

    insert new employee into database

--- Spring Data/_7_SpringDataAutoMapingObjects/ModelMapperLab/ModelMapLab/src/main/java/com/example/modelmaplab/AppMain.java
+++ Spring Data/_7_SpringDataAutoMapingObjects/ModelMapperLab/ModelMapLab/src/main/java/com/example/modelmaplab/AppMain.java
@@ -1,12 +1,16 @@
 package com.example.modelmaplab;
 
 import com.example.modelmaplab.domain.DTO.CreateAddressDTO;
+import com.example.modelmaplab.domain.DTO.CreateEmployeeDTO;
 import com.example.modelmaplab.domain.entities.Address;
 import com.example.modelmaplab.services.AddressService;
+import com.example.modelmaplab.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 @Component
@@ -14,10 +18,12 @@ public class AppMain implements CommandLineRunner {
 
 
     private final AddressService addressService;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public AppMain(AddressService addressService) {
+    public AppMain(AddressService addressService, EmployeeService employeeService) {
         this.addressService = addressService;
+        this.employeeService = employeeService;
     }
 
 
@@ -26,7 +32,33 @@ public class AppMain implements CommandLineRunner {
 
         Scanner scanner = new Scanner(System.in);
 
-        createAddress(scanner);
+        createEmployee(scanner);
+
+        // createAddress(scanner);
+
+
+    }
+
+    private void createEmployee(Scanner scanner) {
+
+        String firstName = scanner.nextLine();
+        BigDecimal salary = new BigDecimal(scanner.nextLine());
+        LocalDate birthday = LocalDate.parse(scanner.nextLine());
+
+//        long addressId = Long.parseLong(scanner.nextLine());
+
+        String country = scanner.nextLine();
+        String city = scanner.nextLine();
+
+        CreateAddressDTO address = new CreateAddressDTO(country, city);
+
+        CreateEmployeeDTO employeeDTO = new CreateEmployeeDTO
+                (firstName, null, salary, birthday, address);
+
+
+        this.employeeService.create(employeeDTO);
+
+        System.out.println(employeeDTO);
 
 
     }
