c7435a1cf7ca9505fd3e7792060826b5ce20e529
Abu <iliyanananan@gmail.com>
2022-11-14 15:41:33 +0200

    upload new file

---
+++ Spring Data/_4_HibernateCodeFirst/FootballBettingDatabase/src/main/java/entities/ResultPredictionValues.java
@@ -0,0 +1,9 @@
+package entities;
+
+public enum ResultPredictionValues {
+    //Prediction (possible values - Home Team Win, Draw Game, Away Team Win)
+    HOME_TEAM_WIN,
+    DRAW_GAE,
+    AWAY_TEAM_WIN;
+
+}
