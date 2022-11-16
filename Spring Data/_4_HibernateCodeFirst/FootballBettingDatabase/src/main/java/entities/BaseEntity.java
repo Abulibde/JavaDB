7b838d9d6a903b64e7f4fc917bdb35bf4b68fb74
Abu <iliyanananan@gmail.com>
2022-11-16 20:30:18 +0200

    upload new file

---
+++ Spring Data/_4_HibernateCodeFirst/FootballBettingDatabase/src/main/java/entities/BaseEntity.java
@@ -0,0 +1,19 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+}
