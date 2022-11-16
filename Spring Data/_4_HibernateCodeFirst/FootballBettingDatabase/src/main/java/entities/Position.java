0046704993e2c7ef19ac01deac68a8c73d903038
Abu <iliyanananan@gmail.com>
2022-11-16 20:31:32 +0200

    upload new file

---
+++ Spring Data/_4_HibernateCodeFirst/FootballBettingDatabase/src/main/java/entities/Position.java
@@ -0,0 +1,16 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class Position extends BaseEntity{
+}
