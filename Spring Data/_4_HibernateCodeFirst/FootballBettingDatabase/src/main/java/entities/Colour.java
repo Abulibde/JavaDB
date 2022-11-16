35e277c3a9661cdeba16fcd5b06d0523755db8b3
Abu <iliyanananan@gmail.com>
2022-11-16 20:30:26 +0200

    upload new file

---
+++ Spring Data/_4_HibernateCodeFirst/FootballBettingDatabase/src/main/java/entities/Colour.java
@@ -0,0 +1,21 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class Colour extends BaseEntity {
+
+    @Column
+    private String name;
+}
+
