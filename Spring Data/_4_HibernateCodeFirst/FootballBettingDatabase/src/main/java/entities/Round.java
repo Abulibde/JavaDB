1aa73e82ca65e1a73072336edf79be345b6f22b0
Abu <iliyanananan@gmail.com>
2022-11-17 17:39:34 +0200

    upload new file

---
+++ Spring Data/_4_HibernateCodeFirst/FootballBettingDatabase/src/main/java/entities/Round.java
@@ -0,0 +1,20 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class Round extends BaseEntity{
+
+    @Column
+    private String name;
+}
