123892cb7adb1447bbf7154299b1800ce69cd9ec
Abu <iliyanananan@gmail.com>
2022-11-17 17:39:18 +0200

    upload new file

--- Spring Data/_4_HibernateCodeFirst/FootballBettingDatabase/src/main/java/entities/Position.java
+++ Spring Data/_4_HibernateCodeFirst/FootballBettingDatabase/src/main/java/entities/Position.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Getter
@@ -12,5 +14,12 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @Entity
 @Table
-public class Position extends BaseEntity{
+public class Position {
+
+    @Id
+    @Column(length = 2)
+    private String id;
+
+    @Column
+    private String description;
 }
