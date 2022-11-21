1b8a2778387bcb27e21c67c8c892869060b24b41
Abu <iliyanananan@gmail.com>
2022-11-21 08:45:11 +0200

    8.	Book Titles Search

--- Spring Data/_6_AdvancedQuerying/AdvQueringEx/src/main/java/com/example/springintro/service/AuthorService.java
+++ Spring Data/_6_AdvancedQuerying/AdvQueringEx/src/main/java/com/example/springintro/service/AuthorService.java
@@ -1,9 +1,11 @@
 package com.example.springintro.service;
 
 import com.example.springintro.model.entity.Author;
+import com.example.springintro.model.entity.Book;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 public interface AuthorService {
     void seedAuthors() throws IOException;
@@ -13,4 +15,6 @@ public interface AuthorService {
     List<String> getAllAuthorsOrderByCountOfTheirBooks();
 
     List<Author> findAllAuthorsNamesEndsWith(String endsWith);
+
+
 }
