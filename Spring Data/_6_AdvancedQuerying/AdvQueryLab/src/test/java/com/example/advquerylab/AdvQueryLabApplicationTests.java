26cb0eba06241577f2acf48157c58e81143af42c
Abu <iliyanananan@gmail.com>
2022-11-24 19:08:21 +0200

    new project

---
+++ Spring Data/_6_AdvancedQuerying/AdvQueryLab/src/test/java/com/example/advquerylab/AdvQueryLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advquerylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvQueryLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
