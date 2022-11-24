64962c7e4cddbcc9d893e1ecfd952e1bc4f664c2
Abu <iliyanananan@gmail.com>
2022-11-24 19:09:00 +0200

    new project

---
+++ Spring Data/_6_AdvancedQuerying/AdvQueryLab/src/main/java/com/example/advquerylab/AdvQueryLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.advquerylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvQueryLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvQueryLabApplication.class, args);
+    }
+
+}
