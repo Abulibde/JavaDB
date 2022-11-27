842173f85be15515ec6132e56e180f649ac14cbf
Abu <iliyanananan@gmail.com>
2022-11-27 18:38:45 +0200

    creation of the table

--- Spring Data/_7_SpringDataAutoMapingObjects/ModelMapperLab/ModelMapLab/src/main/java/com/example/modelmaplab/domain/entities/Address.java
+++ Spring Data/_7_SpringDataAutoMapingObjects/ModelMapperLab/ModelMapLab/src/main/java/com/example/modelmaplab/domain/entities/Address.java
@@ -1,11 +1,19 @@
 package com.example.modelmaplab.domain.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "addresses")
 public class Address {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(nullable = false)
     private String country;
 
+    @Column(nullable = false)
     private String city;
 
     public Address() {
