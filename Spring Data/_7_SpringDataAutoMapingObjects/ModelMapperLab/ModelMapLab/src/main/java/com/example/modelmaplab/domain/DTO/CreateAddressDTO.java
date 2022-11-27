5e32ed767489c2eaf946e1d79f5464d4236828f8
Abu <iliyanananan@gmail.com>
2022-11-27 19:05:37 +0200

    insert new address into database

---
+++ Spring Data/_7_SpringDataAutoMapingObjects/ModelMapperLab/ModelMapLab/src/main/java/com/example/modelmaplab/domain/DTO/CreateAddressDTO.java
@@ -0,0 +1,21 @@
+package com.example.modelmaplab.domain.DTO;
+
+public class CreateAddressDTO {
+
+    private String country;
+
+    private String city;
+
+    public CreateAddressDTO(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
