7ad7d2f4c8d7d47ad615de715dc103f441ef27ab
Abu <iliyanananan@gmail.com>
2022-11-28 10:10:07 +0200

    first try with projection!

--- Spring Data/_7_SpringDataAutoMapingObjects/ModelMapLab/src/main/java/com/example/modelmaplab/repositories/EmployeeRepository.java
+++ Spring Data/_7_SpringDataAutoMapingObjects/ModelMapLab/src/main/java/com/example/modelmaplab/repositories/EmployeeRepository.java
@@ -1,9 +1,18 @@
 package com.example.modelmaplab.repositories;
 
+import com.example.modelmaplab.domain.DTO.EmployeeNamesDTO;
 import com.example.modelmaplab.domain.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+    @Query("SELECT new com.example.modelmaplab.domain.DTO.EmployeeNamesDTO (e.firstName, e.lastName)" +
+            " FROM Employee AS e" +
+            " WHERE e.id = :id")
+    EmployeeNamesDTO findNamesById(long id);
+
 }
