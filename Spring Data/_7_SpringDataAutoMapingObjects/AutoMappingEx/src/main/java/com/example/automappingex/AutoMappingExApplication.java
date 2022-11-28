e03c9d228ecc5c01e3c5fb6a09d9c07febd08731
Abu <iliyanananan@gmail.com>
2022-11-28 16:06:12 +0200

    start of new project about game store

---
+++ Spring Data/_7_SpringDataAutoMapingObjects/AutoMappingEx/src/main/java/com/example/automappingex/AutoMappingExApplication.java
@@ -0,0 +1,13 @@
+package com.example.automappingex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMappingExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoMappingExApplication.class, args);
+    }
+
+}
---
+++ Spring Data/_7_SpringDataAutoMapingObjects/AutoMappingEx/src/test/java/com/example/automappingex/AutoMappingExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automappingex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMappingExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
