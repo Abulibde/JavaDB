0efcd47017f8191f2c4f3268c0717f8df4566fcc
Abu <iliyanananan@gmail.com>
2022-11-30 23:44:38 +0200

    Json project

---
+++ Spring Data/_8_JSONProcessing/JsonExe/src/main/java/com/example/jsonexe/productShop/entities/Product.java
@@ -0,0 +1,46 @@
+package com.example.jsonexe.productShop.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private BigDecimal price;
+
+    @ManyToOne
+    private User buyer;
+
+    @ManyToOne
+    private User seller;
+
+    @ManyToMany
+    private Set<Category> categories;
+
+    public Product() {
+        this.categories = new HashSet<>();
+    }
+
+    public Product(String name, BigDecimal price) {
+        this();
+        this.name = name;
+        this.price = price;
+    }
+}
