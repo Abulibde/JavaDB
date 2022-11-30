b8391116ac905839ba1d371df5a8f07f77c5732b
Abu <iliyanananan@gmail.com>
2022-11-30 23:44:30 +0200

    Json project

---
+++ Spring Data/_8_JSONProcessing/JsonExe/src/test/java/com/example/jsonexe/JsonExeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonexe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonExeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
