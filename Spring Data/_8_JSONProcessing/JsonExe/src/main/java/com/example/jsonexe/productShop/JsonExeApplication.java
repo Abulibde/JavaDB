8bab3908cab57c4205fe4edc77c927f1693a2758
Abu <iliyanananan@gmail.com>
2022-11-30 23:44:23 +0200

    Json project

---
+++ Spring Data/_8_JSONProcessing/JsonExe/src/main/java/com/example/jsonexe/productShop/JsonExeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonexe.productShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonExeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonExeApplication.class, args);
+	}
+
+}
