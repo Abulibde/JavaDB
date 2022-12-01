dba3b292ded5970a6d79b8d8d165688379b006fc
Abu <iliyanananan@gmail.com>
2022-12-02 01:42:13 +0200

    Json project -> Seed the database

---
+++ Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package productsShop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import productsShop.entities.products.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
