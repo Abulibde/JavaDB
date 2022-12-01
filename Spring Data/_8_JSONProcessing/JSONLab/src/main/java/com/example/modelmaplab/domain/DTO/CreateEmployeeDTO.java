5c58b002e2358e3d57e85d0e5e026e24a2c57171
Abu <iliyanananan@gmail.com>
2022-12-01 19:26:05 +0200

    first steps with JSON

--- Spring Data/_8_JSONProcessing/JSONLab/src/main/java/com/example/modelmaplab/domain/DTO/CreateEmployeeDTO.java
+++ Spring Data/_8_JSONProcessing/JSONLab/src/main/java/com/example/modelmaplab/domain/DTO/CreateEmployeeDTO.java
@@ -1,13 +1,25 @@
 package com.example.modelmaplab.domain.DTO;
 
+import com.example.modelmaplab.domain.DTO.addresses.CreateAddressDTO;
+import com.google.gson.annotations.Expose;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class CreateEmployeeDTO {
+    @Expose
     private String firstName;
+
+    @Expose
     private String lastName;
+
+    @Expose
     private BigDecimal salary;
+
+    @Expose
     private LocalDate birthday;
+
+    @Expose
     private CreateAddressDTO address;
 
     public CreateEmployeeDTO(String firstName, String lastName,
