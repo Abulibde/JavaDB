b9243ebbabeec9bff8b093ba538642e97186bf76
Abu <iliyanananan@gmail.com>
2022-12-02 01:42:25 +0200

    Json project -> Seed the database

---
+++ Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/services/SeedService.java
@@ -0,0 +1,19 @@
+package productsShop.services;
+
+import java.io.FileNotFoundException;
+
+public interface SeedService {
+
+    void seedUsers() throws FileNotFoundException;
+
+    void seedCategories() throws FileNotFoundException;
+
+    void seedProducts() throws FileNotFoundException;
+
+    default void seedAll() throws FileNotFoundException {
+        seedUsers();
+        seedCategories();
+        seedProducts();
+    }
+
+}
