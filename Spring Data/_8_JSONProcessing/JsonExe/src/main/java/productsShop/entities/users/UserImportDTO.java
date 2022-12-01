4f39ef1a1e9099c8456df41ce8e09706ea440ceb
Abu <iliyanananan@gmail.com>
2022-12-02 01:42:45 +0200

    Json project -> Seed the database

---
+++ Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/entities/users/UserImportDTO.java
@@ -0,0 +1,14 @@
+package productsShop.entities.users;
+
+
+import lombok.Getter;
+
+@Getter
+public class UserImportDTO {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+
+}
