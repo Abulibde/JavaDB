29af4af76092a7b70f6ea01737069e24f1fe71fc
Abu <iliyanananan@gmail.com>
2022-12-02 01:42:07 +0200

    Json project -> Seed the database

---
+++ Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/entities/products/ProductImportDTO.java
@@ -0,0 +1,13 @@
+package productsShop.entities.products;
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+@Getter
+public class ProductImportDTO {
+
+    private String name;
+
+    private BigDecimal price;
+
+}
