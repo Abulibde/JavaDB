3c3e6dee3c40d589d862d7ecd0f985252426d401
Abu <iliyanananan@gmail.com>
2022-12-02 10:22:50 +0200

    Json project -> #3

--- Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/repositories/ProductRepository.java
+++ Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/repositories/ProductRepository.java
@@ -1,7 +1,9 @@
 package productsShop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import productsShop.entities.categories.CategoryStatsDTO;
 import productsShop.entities.products.Product;
 import productsShop.entities.products.ProductWithoutBuyerDTO;
 
@@ -11,4 +13,12 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
     List<ProductWithoutBuyerDTO> findAllByPriceBetweenAndBuyerIsNullOrderByPriceAsc(BigDecimal rangeStart, BigDecimal rangeEnd);
+
+
+    @Query("SELECT new productsShop.entities.categories.CategoryStatsDTO(" +
+            "c.name, COUNT(p), AVG(p.price), SUM(p.price))" +
+            " FROM Product p" +
+            " JOIN p.categories AS c" +
+            " GROUP BY c")
+    List<CategoryStatsDTO> getCategoryStats();
 }
