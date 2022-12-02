f131b8e8d9bbe1b3adfb4477dc5af7a06ed21be3
Abu <iliyanananan@gmail.com>
2022-12-02 02:15:17 +0200

    Json project -> Seed the database

--- Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/repositories/ProductRepository.java
+++ Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/repositories/ProductRepository.java
@@ -3,7 +3,12 @@ package productsShop.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import productsShop.entities.products.Product;
+import productsShop.entities.products.ProductWithoutBuyerDTO;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+    List<ProductWithoutBuyerDTO> findAllByPriceBetweenAndBuyerIsNullOrderByPriceAsc(BigDecimal rangeStart, BigDecimal rangeEnd);
 }
