9da4a004633a4999abf09c285147bdecd5dd23ed
Abu <iliyanananan@gmail.com>
2022-12-02 02:16:15 +0200

    Json project -> Seed the database

--- Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/ProductsShopRunner.java
+++ Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/ProductsShopRunner.java
@@ -2,20 +2,28 @@ package productsShop;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import productsShop.services.ProductService;
 import productsShop.services.SeedService;
+
+import java.math.BigDecimal;
+
 @Component
 public class ProductsShopRunner implements CommandLineRunner {
 
     private final SeedService seedService;
+    private final ProductService productService;
 
-    public ProductsShopRunner(SeedService seedService) {
+    public ProductsShopRunner(SeedService seedService, ProductService productService) {
         this.seedService = seedService;
+        this.productService = productService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
 
+        this.productService.getProductsInPriceRangeForSell(500, 1000)
+                .forEach(productWithoutBuyerDTO -> System.out.println(productWithoutBuyerDTO.getSeller()));
         //this.seedService.seedAll();
     }
 }
