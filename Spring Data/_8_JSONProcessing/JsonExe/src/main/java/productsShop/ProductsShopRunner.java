22b105deff96d48ba8ab76709f31ef5b03e842e3
Abu <iliyanananan@gmail.com>
2022-12-02 10:23:07 +0200

    Json project -> #3

--- Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/ProductsShopRunner.java
+++ Spring Data/_8_JSONProcessing/JsonExe/src/main/java/productsShop/ProductsShopRunner.java
@@ -1,11 +1,14 @@
 package productsShop;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import productsShop.entities.categories.CategoryStatsDTO;
 import productsShop.services.ProductService;
 import productsShop.services.SeedService;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 public class ProductsShopRunner implements CommandLineRunner {
@@ -13,17 +16,31 @@ public class ProductsShopRunner implements CommandLineRunner {
     private final SeedService seedService;
     private final ProductService productService;
 
+    private Gson gson;
+
     public ProductsShopRunner(SeedService seedService, ProductService productService) {
         this.seedService = seedService;
         this.productService = productService;
+        this.gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
     }
 
 
     @Override
     public void run(String... args) throws Exception {
 
-        this.productService.getProductsInPriceRangeForSell(500, 1000)
-                .forEach(productWithoutBuyerDTO -> System.out.println(productWithoutBuyerDTO.getSeller()));
-        //this.seedService.seedAll();
+        List<CategoryStatsDTO> categoryStatistics = this.productService.getCategoryStatistics();
+
+        String json = this.gson.toJson(categoryStatistics);
+
+        System.out.println(json);
+
+
+        //        this.productService.getProductsInPriceRangeForSell(500, 1000)
+//                .forEach(productWithoutBuyerDTO -> System.out.println(productWithoutBuyerDTO.getSeller()));
+
+
+//        this.seedService.seedAll();
     }
 }
