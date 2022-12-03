eba04f3e5b38ccadb1053317c8363a847afc9a6d
Abu <iliyanananan@gmail.com>
2022-12-03 08:16:53 +0200

    end of the ExamPreparation

--- Spring Data/_10_ExamPreparation/ExamPreparationLecture/src/main/java/com/example/football/repository/PlayerRepository.java
+++ Spring Data/_10_ExamPreparation/ExamPreparationLecture/src/main/java/com/example/football/repository/PlayerRepository.java
@@ -1,6 +1,18 @@
 package com.example.football.repository;
 
-//ToDo:
-public interface PlayerRepository  {
+import com.example.football.models.dto.ImportPlayerDTO;
+import com.example.football.models.entity.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    Optional<Player> findByEmail(String email);
+
+    List<Player> findByBirthDateBetweenOrderByStatShootingDescStatPassingDescStatEnduranceDescLastNameAsc(LocalDate after, LocalDate before);
 }
