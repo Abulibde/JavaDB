a3a67653ed0a727922a8cee7239131ad1ec0a241
Abu <iliyanananan@gmail.com>
2022-12-09 17:03:22 +0200

    # 17. Highest Peak and Longest River by Country

--- MySql/6) Subqueries and JOINs/geography_database.sql
+++ MySql/6) Subqueries and JOINs/geography_database.sql
@@ -870,4 +870,16 @@ FROM countries
          LEFT JOIN mountains_countries mc on countries.country_code = mc.country_code
 WHERE mc.mountain_id IS NULL;
 
+# 17. Highest Peak and Longest River by Country
+#For each country, find the elevation of the highest peak and the length of the longest river,
+SELECT c.country_name, MAX(p.elevation) AS 'highest_peak_elevation', MAX(r.length) AS 'longest_river_length'
+FROM countries AS c
+         LEFT JOIN mountains_countries AS mc USING (country_code)
+         LEFT JOIN peaks AS p USING (mountain_id)
+         LEFT JOIN countries_rivers AS cr USING (country_code)
+         LEFT JOIN rivers AS r ON cr.river_id = r.id
+GROUP BY c.country_name
+ORDER BY highest_peak_elevation DESC, longest_river_length DESC, c.country_name
+LIMIT 5;
+
 
