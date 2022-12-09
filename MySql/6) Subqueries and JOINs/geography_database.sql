71b601d518318aab40b9e0e26c23027dbdb1b6cf
Abu <iliyanananan@gmail.com>
2022-12-09 16:27:27 +0200

    # 13.	Count Mountain Ranges

--- MySql/6) Subqueries and JOINs/geography_database.sql
+++ MySql/6) Subqueries and JOINs/geography_database.sql
@@ -847,4 +847,12 @@ WHERE mc.country_code = 'BG'
   AND p.elevation > 2835
 ORDER BY p.elevation DESC;
 
+# 13.	Count Mountain Ranges
+SELECT mc.country_code, COUNT(m.mountain_range) AS 'mountain_range_count'
+FROM mountains AS m
+JOIN mountains_countries AS mc ON m.id = mc.mountain_id
+WHERE mc.country_code IN ('BG', 'US', 'RU')
+GROUP BY mc.country_code
+ORDER BY 'mountain_range_count' DESC;
+
 
