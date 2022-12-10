65d594659c1d95eb35ae9b997befaab935f21dca
Abu <iliyanananan@gmail.com>
2022-12-10 16:47:03 +0200

    # 11.	Movie awards

--- MySql/8)ExamPreaparation/ExamPreparationDesi.sql
+++ MySql/8)ExamPreaparation/ExamPreparationDesi.sql
@@ -172,6 +172,15 @@ BEGIN
     RETURN history_movies;
 END;
 
+# 11.	Movie awards
+CREATE PROCEDURE udp_award_movie(movie_title VARCHAR(50))
+BEGIN
+    UPDATE actors AS a
+        JOIN movies_actors AS ma ON a.id = ma.actor_id
+        JOIN movies AS m ON ma.movie_id = m.id
+    SET a.awards = a.awards + 1
+    WHERE m.title = movie_title;
+END;
 
 
 
