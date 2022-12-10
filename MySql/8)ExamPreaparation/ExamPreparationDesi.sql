ca334836a9e6bd3a2be8dcf880cb4428a1c4161f
Abu <iliyanananan@gmail.com>
2022-12-10 16:27:02 +0200

    09.	Rating system

--- MySql/8)ExamPreaparation/ExamPreparationDesi.sql
+++ MySql/8)ExamPreaparation/ExamPreparationDesi.sql
@@ -143,6 +143,19 @@ HAVING movies_count >= 7
 ORDER BY c.name DESC;
 
 # 09.	Rating system
+SELECT m.title,
+       CASE
+           WHEN mai.rating <= 4 THEN 'poor'
+           WHEN mai.rating <= 7 THEN 'good'
+           ELSE 'excellent'
+           END                               AS rating,
+       IF(mai.has_subtitles, 'english', '-') AS subtitles,
+       mai.budget
+FROM movies_additional_info AS mai
+         JOIN movies m on mai.id = m.movie_info_id
+ORDER BY mai.budget DESC;
+
+#
 
 
 
